d49a96f30c6ecfcb4160266f6481cd74c2c0e74c
Stephen von Takach <steve@place.technology>
2025-04-08 15:12:39 +1000

    chore: cleanup database before running specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -9,7 +9,6 @@ require "micrate"
 require "pg"
 Micrate::DB.connection_url = App::PG_DATABASE_URL
 Micrate::Cli.run_status
-# Micrate::Cli.drop_database rescue nil
 Micrate::Cli.run_up
 
 # Your application config
